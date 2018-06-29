ee8d77ff2a510700a3a8069db120cfec9638087f
TheArsenik <thearsenik@gmail.com>
2018-06-30 00:04:50 +0200

    Usable version to design quarry map
    Use arrows to put a new block, then fill inputs (switch with Tab or
    Enter key) and then do it again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Renderer, ViewChild, HostBinding} from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public static readonly STATE_NEXT_INFOS = "infos";
+  public static readonly STATE_NEXT_DIRECTION = "direction";
+
+  state = AppComponent.STATE_NEXT_DIRECTION;
+
+  @ViewChild('focusInput') focusInput:ElementRef;
+
+  // Form
+  height:number = 0;
+  heightDiff:number = 0;
+  
+  @ViewChild('heightDiffInput') heightDiffInput:ElementRef;
+  @ViewChild('heightInput') heightInput:ElementRef;
+
+  // Draw
+  path = [];
+  currentBlockId = 0;
+  currentBlockX = 0;
+  currentBlockY = 5;
+  blockSize = 10;
+
+  constructor(private renderer: Renderer) {
+
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (this.state === AppComponent.STATE_NEXT_DIRECTION) {
+      let knownKey = false;
+      if (event.keyCode === KEY_CODE.LEFT_ARROW) {
+        this.currentBlockX--;
+        knownKey = true;
+      } else if(event.keyCode === KEY_CODE.TOP_ARROW) {
+        this.currentBlockY--;
+        knownKey = true;
+      } else if(event.keyCode === KEY_CODE.RIGHT_ARROW) {
+        this.currentBlockX++;
+        knownKey = true;
+      } else if(event.keyCode === KEY_CODE.BOTTOM_ARROW) {
+        this.currentBlockY--;
+        knownKey = true;
+      }
+  
+      if (knownKey) {
+        this.state = AppComponent.STATE_NEXT_INFOS;
+        this.setFocus(this.heightInput);
+      }
+    }
+  }
+
+  setFocus(input:ElementRef) {
+    setTimeout(() => {
+      this.renderer.invokeElementMethod(input.nativeElement, 'focus');
+    }, 500);
+  }
+
+  validHeight() {
+    this.setFocus(this.heightDiffInput);
+  }
+
+  validHeightDiff() {
+    let lastBlock = null;
+    if (this.path.length > 0) {
+      lastBlock = this.path[this.path.length - 1];
+    }
+    this.path.push({
+      x: this.currentBlockX,
+      y: this.currentBlockY,
+      height: this.height,
+      heightDiff: this.heightDiff,
+      previous: lastBlock,
+    });
+    this.state = AppComponent.STATE_NEXT_DIRECTION;
+    this.setFocus(this.focusInput);
+  }
+}
+
+export enum KEY_CODE {
+  LEFT_ARROW = 37,
+  TOP_ARROW = 38,
+  RIGHT_ARROW = 39,
+  BOTTOM_ARROW = 40
+}
+
+export interface Block {
+  x: number;
+  y: number;
+  height:number;
+  heightDiff: number;
+  previous?: Block;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {FormsModule} from '@angular/forms/';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
